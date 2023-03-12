c0d0161ba3125acf20353cea5f09b34072b55f35
Maximov Oleg <smegovic@gmail.com>
2023-03-13 00:43:25 +0500

    Реализация парсера для StackOverflow

---
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/stackoverflow/StackOverflowLinkParser.java
@@ -0,0 +1,36 @@
+package ru.tinkoff.edu.java.linkparser.stackoverflow;
+
+import ru.tinkoff.edu.java.linkparser.common.AbstractLinkParser;
+import ru.tinkoff.edu.java.linkparser.stackoverflow.payload.StackOverflowPayload;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+/**
+ * Парсер ссылок ресурса stackoverflow.com
+ */
+public final class StackOverflowLinkParser<T> extends AbstractLinkParser {
+
+    public static final Pattern PATTERN_STACKOVERFLOW_QUESTION_ID =
+            Pattern.compile("^[a-z]+://stackoverflow[.]com/questions/(?<questionId>\\d+)");
+
+    private final Result.Processor<T, StackOverflowPayload> resultProcessor;
+
+    public StackOverflowLinkParser(final Result.Processor<T, StackOverflowPayload> resultProcessor) {
+        this.resultProcessor = Objects.requireNonNull(resultProcessor);
+    }
+
+    @Override
+    public Result doInternalParse(final String link) {
+
+        final var matcher = PATTERN_STACKOVERFLOW_QUESTION_ID.matcher(link);
+
+        if (matcher.find()) {
+            final var questionId = Long.parseLong(matcher.group("questionId"));
+
+            return Result.success(new StackOverflowPayload(questionId), this.resultProcessor);
+        }
+
+        return Result.failed();
+    }
+}
---
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/stackoverflow/payload/StackOverflowPayload.java
@@ -0,0 +1,4 @@
+package ru.tinkoff.edu.java.linkparser.stackoverflow.payload;
+
+public record StackOverflowPayload(Long questionId) {
+}
