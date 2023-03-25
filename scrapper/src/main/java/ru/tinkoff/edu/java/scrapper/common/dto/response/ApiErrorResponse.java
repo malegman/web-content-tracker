e4cd360fd3c97f1f9164757775bb0192e4f9cb15
Maximov Oleg <smegovic@gmail.com>
2023-03-25 19:30:35 +0500

    Scrapper. Подготовка инфраструктуры выполнения запросов

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/common/dto/response/ApiErrorResponse.java
@@ -0,0 +1,20 @@
+package ru.tinkoff.edu.java.scrapper.common.dto.response;
+
+import java.util.List;
+
+/**
+ * Ответ API для возврата ошибки выполнения
+ *
+ * @param description      описание ошибки
+ * @param code             код ошибки (идентификатор)
+ * @param exceptionName    наименование ошибки
+ * @param exceptionMessage сообщение ошибки
+ * @param stacktrace       stack trace ошибки
+ */
+public record ApiErrorResponse(String description, String code, String exceptionName,
+                               String exceptionMessage, List<String> stacktrace) {
+
+    public ApiErrorResponse(String exceptionMessage) {
+        this(null, null, null, exceptionMessage, null);
+    }
+}
---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/common/spring/mvc/AbstractScrapperHandlerFunction.java
@@ -0,0 +1,100 @@
+package ru.tinkoff.edu.java.scrapper.common.spring.mvc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionOperations;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+import ru.tinkoff.edu.java.scrapper.common.dto.response.ApiErrorResponse;
+import ru.tinkoff.edu.java.scrapper.common.validation.Validation;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * Абстрактный обработчик HTTP запросов, реализует spring интерфейс {@link HandlerFunction}
+ */
+@Slf4j
+public abstract class AbstractScrapperHandlerFunction implements HandlerFunction<ServerResponse> {
+
+    private final TransactionOperations transactionOperations;
+
+    public AbstractScrapperHandlerFunction(final TransactionOperations transactionOperations) {
+        this.transactionOperations = Objects.requireNonNull(transactionOperations);
+    }
+
+    private ServerResponse serverApiErrorResponse(ApiErrorResponse apiErrorResponse) {
+        return ServerResponse
+                .badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(apiErrorResponse);
+    }
+
+    /**
+     * Обрабатывает http запрос в рамках транзакции
+     *
+     * @param request обрабатываемый запрос
+     * @param status  статус транзакции
+     *
+     * @return http ответ
+     *
+     * @throws Exception если произошла ошибка обработки запроса
+     */
+    protected abstract ServerResponse handleInternal(ServerRequest request, TransactionStatus status) throws Exception;
+
+    /**
+     * Обрабатывает http запрос
+     *
+     * @param request обрабатываемый запрос
+     *
+     * @return http ответ
+     */
+    @Override
+    public ServerResponse handle(final ServerRequest request) {
+
+        try {
+            return this.transactionOperations.execute(status -> {
+                try {
+                    return this.handleInternal(request, status);
+                } catch (Exception exception) {
+                    throw new RuntimeException(exception);
+                }
+            });
+        } catch (Exception exception) {
+            final var exceptionId = UUID.randomUUID();
+            final var exceptionMessage = exception.getMessage();
+            log.error("Exception id: {}. Exception message: {}", exceptionId, exceptionMessage);
+
+            return this.serverApiErrorResponse(new ApiErrorResponse(
+                    exceptionMessage,
+                    exceptionId.toString(),
+                    exception.getClass().getName(),
+                    exceptionMessage,
+                    Arrays.stream(exception.getStackTrace())
+                            .map(StackTraceElement::toString)
+                            .toList()));
+        }
+    }
+
+    /**
+     * Преобразует объект валидации в http ответ со статусом 400
+     *
+     * @param validation объект валидации
+     *
+     * @return http ответ со статусом 400
+     */
+    protected ServerResponse badRequestFromValidation(final Validation validation) {
+        return this.serverApiErrorResponse(new ApiErrorResponse(
+                validation.getFieldErrors()
+                        .entrySet()
+                        .stream()
+                        .map(entry -> String.format("Field: %s. Errors: %s.",
+                                entry.getKey(),
+                                entry.getValue()))
+                        .collect(Collectors.joining("],[", "[", "]"))));
+    }
+}
