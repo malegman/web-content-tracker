c272e2851afe45272b67aca847eea5e5da07d0de
Maximov Oleg <smegovic@gmail.com>
2023-03-27 18:39:44 +0500

    Bot. Тестирование

---
+++ bot/src/test/java/ru/tinkoff/edu/java/bot/application/client/api/spring/web/SendUpdatesHandlerFunctionIT.java
@@ -0,0 +1,99 @@
+package ru.tinkoff.edu.java.bot.application.client.api.spring.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.tinkoff.edu.java.bot.common.ApiErrorResponseAssertionMatcher;
+import ru.tinkoff.edu.java.bot.common.errors.ErrorType;
+import ru.tinkoff.edu.java.bot.common.errors.ValidationFailedException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SendUpdatesHandlerFunctionIT {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void sendUpdates_RequestIsValid_ReturnsResponseWithStatusNoContent() throws Exception {
+
+        final var requestBuilder = post("/updates")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "id": 1,
+                            "url": "url",
+                            "description": "description",
+                            "tgChatIds": [1, 2]
+                        }
+                        """);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void sendUpdates_RequestIsInvalid_ReturnsResponseWithStatusBadRequest() throws Exception {
+
+        final var requestBuilder = post("/updates")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "url": " ",
+                            "tgChatIds": [1, null]
+                        }
+                        """);
+
+        final var matcher = new ApiErrorResponseAssertionMatcher(this.objectMapper);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(matcher));
+
+        final var apiErrorResponse = matcher.getApiErrorResponse();
+        Assertions.assertNotNull(apiErrorResponse);
+        Assertions.assertEquals(apiErrorResponse.description(), ErrorType.VALIDATION_FAILED.getDescription());
+        Assertions.assertEquals(apiErrorResponse.code(), ErrorType.VALIDATION_FAILED.getCode());
+        Assertions.assertEquals(4, apiErrorResponse.exceptionMessage().split(";").length);
+        Assertions.assertEquals(apiErrorResponse.exceptionName(), ValidationFailedException.class.getName());
+    }
+
+    @Test
+    void sendUpdates_IncorrectInput_ReturnsResponseWithStatusBadRequest() throws Exception {
+
+        final var requestBuilder = post("/updates")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "id": "id",
+                            "url": "url",
+                            "description": "description",
+                            "tgChatIds": [1, 2]
+                        }
+                        """);
+
+        final var matcher = new ApiErrorResponseAssertionMatcher(this.objectMapper);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(matcher));
+
+        final var apiErrorResponse = matcher.getApiErrorResponse();
+        Assertions.assertNotNull(apiErrorResponse);
+        Assertions.assertEquals(apiErrorResponse.description(), ErrorType.EXECUTION_FAILED.getDescription());
+        Assertions.assertEquals(apiErrorResponse.code(), ErrorType.EXECUTION_FAILED.getCode());
+    }
+}
---
+++ bot/src/test/java/ru/tinkoff/edu/java/bot/application/client/usecase/SendUpdatesUseCaseTest.java
@@ -0,0 +1,136 @@
+package ru.tinkoff.edu.java.bot.application.client.usecase;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.tinkoff.edu.java.bot.application.client.api.SendUpdatesApi.Payload;
+import ru.tinkoff.edu.java.bot.application.client.api.SendUpdatesApi.Result;
+import ru.tinkoff.edu.java.bot.application.shared.application.spi.SendUpdatesSpi;
+import ru.tinkoff.edu.java.bot.application.shared.domain.id.LinkId;
+import ru.tinkoff.edu.java.bot.application.shared.domain.id.TgChatId;
+import ru.tinkoff.edu.java.bot.common.validation.Validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SendUpdatesUseCaseTest {
+
+    @Mock
+    SendUpdatesSpi sendUpdatesSpi;
+
+    SendUpdatesUseCase useCase;
+
+    @BeforeEach
+    void setUp() {
+        this.useCase = new SendUpdatesUseCase(this.sendUpdatesSpi);
+    }
+
+    @Test
+    void sendUpdates_PayloadIsValid_ReturnsSuccessResult() {
+
+        // given
+        final var id = new LinkId(1);
+        final var url = "url";
+        final var description = "description";
+        final var tgChatIds = List.of(new TgChatId(1), new TgChatId(2));
+
+        doNothing().when(this.sendUpdatesSpi).sendUpdates(id, url, description, tgChatIds);
+
+        // when
+        final var result = this.useCase.invoke(builder -> builder
+                .id(id)
+                .url(url)
+                .description(description)
+                .tgChatIds(tgChatIds));
+
+        // then
+        assertEquals(Result.success(), result);
+        verifyNoMoreInteractions(this.sendUpdatesSpi);
+    }
+
+    @Test
+    void sendUpdates_PayloadIsInvalid_ReturnsValidationFailedResult() {
+
+        // given
+        final var id = LinkId.valueOf(null);
+        final var url = "  ";
+        final var description = "";
+        final var tgChatIds = new ArrayList<>(List.of(new TgChatId(1)));
+        tgChatIds.add(null);
+
+        // when
+        final var result = this.useCase.invoke(builder -> builder
+                .id(id)
+                .url(url)
+                .description(description)
+                .tgChatIds(tgChatIds));
+
+        // then
+        assertEquals(Result.validationFailed(new Validation(Map.of(
+                Payload.PROP_ID,
+                List.of(Payload.ERROR_ID),
+                Payload.PROP_URL,
+                List.of(Payload.ERROR_URL),
+                Payload.PROP_DESCRIPTION,
+                List.of(Payload.ERROR_DESCRIPTION),
+                Payload.PROP_TG_CHAT_IDS,
+                List.of(Payload.ERROR_TG_CHAT_IDS)
+        ))), result);
+        verifyNoInteractions(this.sendUpdatesSpi);
+    }
+
+    @Test
+    void sendUpdates_EmptyTgChatIds_ReturnsValidationFailedResult() {
+
+        // given
+        final var id = new LinkId(1);
+        final var url = "url";
+        final var description = "description";
+        final var tgChatIds = List.<TgChatId>of();
+
+        // when
+        final var result = this.useCase.invoke(builder -> builder
+                .id(id)
+                .url(url)
+                .description(description)
+                .tgChatIds(tgChatIds));
+
+        // then
+        assertEquals(Result.validationFailed(new Validation(Map.of(
+                Payload.PROP_TG_CHAT_IDS,
+                List.of(Payload.ERROR_TG_CHAT_IDS)
+        ))), result);
+        verifyNoInteractions(this.sendUpdatesSpi);
+    }
+
+    @Test
+    void sendUpdates_ThrowsException_ReturnsExecutionFailedResult() {
+
+        // given
+        final var id = new LinkId(1);
+        final var url = "url";
+        final var description = "description";
+        final var tgChatIds = List.of(new TgChatId(1), new TgChatId(2));
+
+        final var exception = new RuntimeException();
+        doThrow(exception).when(this.sendUpdatesSpi).sendUpdates(id, url, description, tgChatIds);
+
+        // when
+        final var result = this.useCase.invoke(builder -> builder
+                .id(id)
+                .url(url)
+                .description(description)
+                .tgChatIds(tgChatIds));
+
+        // then
+        assertEquals(Result.executionFailed(exception), result);
+        verifyNoMoreInteractions(this.sendUpdatesSpi);
+    }
+}
