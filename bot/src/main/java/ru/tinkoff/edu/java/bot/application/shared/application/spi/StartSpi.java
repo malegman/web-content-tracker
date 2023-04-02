df4abc8ae316d16ee34b39fdaf1e9d64e03649c5
Maximov Oleg <smegovic@gmail.com>
2023-04-02 13:33:45 +0500

    Bot. Изменение структуры обработки запросов бота

--- bot/src/main/java/ru/tinkoff/edu/java/bot/application/shared/application/spi/StartSpi.java
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.edu.java.bot.application.shared.application.spi;
-
-import ru.tinkoff.edu.java.bot.application.shared.domain.id.TgChatId;
-
-@FunctionalInterface
-public interface StartSpi {
-
-    void start(TgChatId tgChatId);
-}
