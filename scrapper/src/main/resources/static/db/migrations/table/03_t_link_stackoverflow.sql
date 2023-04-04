bb295c90ec6e6f5eb3809d7920546f4c122256d9
Maximov Oleg <smegovic@gmail.com>
2023-04-04 16:28:01 +0500

    Scrapper. Добавил поле c_question_id в таблицу t_link_stackoverflow

--- scrapper/src/main/resources/static/db/migrations/table/03_t_link_stackoverflow.sql
+++ scrapper/src/main/resources/static/db/migrations/table/03_t_link_stackoverflow.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS scrapper.t_link_stackoverflow (
     id BIGSERIAL NOT NULL,
     c_link VARCHAR(255) NOT NULL,
+    c_question_id BIGINT NOT NULL,
     c_question_tags VARCHAR(50) [],
     c_question_is_answered BOOLEAN,
     c_question_last_activity_date TIMESTAMP WITH TIME ZONE,
