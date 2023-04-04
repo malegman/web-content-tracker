af9ed9c6fbbb345a01dd37a69fbaaab931f5fc2a
Maximov Oleg <smegovic@gmail.com>
2023-04-04 19:35:38 +0500

    Scrapper. Добавил в IT тесты управление транзакцией БД

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/application/links/api/spring/web/FindLinksHandlerFunctionIT.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/application/links/api/spring/web/FindLinksHandlerFunctionIT.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import ru.tinkoff.edu.java.scrapper.common.ApiErrorResponseAssertionMatcher;
 import ru.tinkoff.edu.java.scrapper.common.errors.ErrorType;
 import ru.tinkoff.edu.java.scrapper.common.errors.ValidationFailedException;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class FindLinksHandlerFunctionIT {
 
     @Autowired
